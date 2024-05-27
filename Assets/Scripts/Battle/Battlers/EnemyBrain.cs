38383f2d53e3f23a08c7224f5fc160a4d5df069e
Javier Murillo González <javmuril@ucm.es>
2024-05-27 18:12:21 +0200

    Estructura de acciones

--- Assets/Scripts/Battle/Battlers/EnemyBrain.cs
+++ Assets/Scripts/Battle/Battlers/EnemyBrain.cs
@@ -2,11 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FuzzyLogicSystem;
+using System.Linq;
 
 public class EnemyBrain : MonoBehaviour
 {
     public TextAsset fuzzyLogicData = null;
     public FuzzyLogic fuzzyLogic { get; private set; } = null;
+
+    //Struct para almacenar las acciones y los valores post fuzzi
+    private struct Action
+    {
+        public string name;
+        public float value;
+
+        public Action(string name, float value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+        public static bool operator >(Action a, Action b)
+        {
+            return a.value > b.value;
+        }
+        public static bool operator <(Action a, Action b)
+        {
+            return a.value < b.value;
+        }
+    }
+
+    
+    //set de acciones para tenerlas ordenadas de mayor a menor, y asi sacar la mas prioritaria
+    HashSet<Action> action; 
+
+    bool isTurn = false;
     
     [SerializeField] Enemy enemy;
     // Start is called before the first frame update
@@ -19,7 +47,12 @@ public class EnemyBrain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (isTurn)
+        {
+            UpdateValues();
+            CalculateData();
+            doAction();
+        }
     }
 
     void UpdateValues()
@@ -44,6 +77,45 @@ public class EnemyBrain : MonoBehaviour
     {
         UpdateValues();
         //recoger inferencias
+        //valor de 0 a 1
+        for(int i = 0; i < fuzzyLogic.NumberInferences(); i++)
+        {
+            action.Add(new Action(fuzzyLogic.GetInference(i).name, fuzzyLogic.GetInference(i).Output()));
+        }
+        //valor a devolver
+        //action.First();
             
     }
+
+    void doAction()
+    {
+
+        switch (action.First().name)
+        {
+            case "Attack":
+                //hacer ataque
+                break;
+            case "Defend":
+                //hacer defensa
+                break;
+            case "Heal":
+                //hacer curacion
+                break;
+            case "Buff":
+                //hacer buff
+                break;
+            case "Debuff":
+                //hacer debuff
+                break;
+            case "Flee":
+                //hacer huida
+                break;
+            default:
+                break;
+        }
+
+        isTurn = false;
+    }
+
+   
 }
